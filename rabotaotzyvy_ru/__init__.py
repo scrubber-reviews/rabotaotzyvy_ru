19a9ae77f86d732fc670aaa50daafbd4e9b6a452
Melis <melis.zhoroev@gmail.com>
2019-07-29 04:55:32 +0700

    friendly import provider

--- rabotaotzyvy_ru/__init__.py
+++ rabotaotzyvy_ru/__init__.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
 """Top-level package for Rabota Otzyvy."""
+from .rabotaotzyvy_ru import RabotaOtzyvyRu
 
 __author__ = """NMelis"""
 __email__ = 'melis.zhoroev@gmail.com'
 __version__ = '0.1.0'
+
+provider = RabotaOtzyvyRu
